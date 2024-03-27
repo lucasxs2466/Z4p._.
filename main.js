c1f9154d67fb7669441c0703ee383d2ac7dfab75
lucasxs2466 <130997187+lucasxs2466@users.noreply.github.com>
2024-03-27 08:14:31 -0300

    .

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ alert('Bem vindo');
 let nomeUsuario = '';
 let texto = document.querySelector('span');
 
-texto.textContent = nomeUsuario;
-
 while (nomeUsuario == ''){
  nomeUsuario = prompt('qual é o seu nome?');
 }
+
+texto.textContent = nomeUsuario;
