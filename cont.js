8115a058f0edc646a2f6779070828f8d6e165b41
lucasxs2466 <130997187+lucasxs2466@users.noreply.github.com>
2024-04-03 08:40:25 -0300

    .

--- cont.js
+++ cont.js
@@ -3,5 +3,10 @@ const botoes = document.querySelectorAll('.Objetivo');
 
 for (let i = 0; i < botoes.length; i++){
 console.log(i);
+botoes[i].onclick = function(){
+    botoes[i].classList.add('ativo');
+}
+
+//botoes[i].classList.remove('ativo');
 }
 
