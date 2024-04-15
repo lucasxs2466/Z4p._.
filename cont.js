e1bbd9614295f9d5dc62a95ef5f154e6ddb2c61b
lucasxs2466 <130997187+lucasxs2466@users.noreply.github.com>
2024-04-15 12:00:18 -0300

    .

--- cont.js
+++ cont.js
@@ -1,5 +1,5 @@
 const botoes = document.querySelectorAll('.Objetivo');
-const texto = document.querySelectorALL('.aba-conteudo')
+const texto = document.querySelectorALL('.aba-conteudo');
 
 for (let i = 0; i < botoes.length; i++){
 console.log(i);
@@ -16,7 +16,7 @@ botoes[i].onclick = function(){
 //botoes[i].classList.remove('ativo');
 }
 
-const contadores = documentSelectorAll('.contador')
+const contadores = documentSelectorAll('.contador');
 const TempoObjetivo1 = new Date ('2024-04-16T00:00:00');
 
 let TempoAtual = New Date ();
