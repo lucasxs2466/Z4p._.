fbaea03da668cf6e2e3153c37fa3d85f4ced17fd
lucasxs2466 <130997187+lucasxs2466@users.noreply.github.com>
2024-04-15 11:59:39 -0300

    .

--- cont.js
+++ cont.js
@@ -6,10 +6,10 @@ console.log(i);
 botoes[i].onclick = function(){
     for(let j = 0; j< botoes.length; j++){
         botoes[j].classList.remove('ativo');
- texto[j].classList.remove('ativa') 
+ texto[j].classList.remove('ativa');
     }
     botoes[i].classList.add('ativo');
-    texto[i].classList.add('ativa')
+    texto[i].classList.add('ativa');
 }
 
 
