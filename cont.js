44f2bacb2af21f5fcf8a9d6a023a5cceec1ee75e
lucasxs2466 <130997187+lucasxs2466@users.noreply.github.com>
2024-04-15 12:02:50 -0300

    .

--- cont.js
+++ cont.js
@@ -1,12 +1,14 @@
 const botoes = document.querySelectorAll('.Objetivo');
 const texto = document.querySelectorALL('.aba-conteudo');
+//botoes.lenght
+
 
 for (let i = 0; i < botoes.length; i++){
 console.log(i);
-botoes[i].onclick = function(){
+   botoes[i].onclick = function(){
     for(let j = 0; j< botoes.length; j++){
         botoes[j].classList.remove('ativo');
- texto[j].classList.remove('ativa');
+        texto[j].classList.remove('ativa');
     }
     botoes[i].classList.add('ativo');
     texto[i].classList.add('ativa');
