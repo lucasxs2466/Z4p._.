6cd1873699e3bf2ad05260ecc941bd7c55c5cf23
lucasxs2466 <130997187+lucasxs2466@users.noreply.github.com>
2024-04-15 12:11:52 -0300

    ;

--- cont.js
+++ cont.js
@@ -21,4 +21,4 @@ const TempoObjetivo1 = new Date ('2024-04-16T00:00:00');
 
 let TempoAtual = new Date ();
 
-contadores[0].textContent = TempoAtual - TempoObjetivo1;
+contadores[0].textContent =TempoObjetivo1 - TempoAtual;
