ade29f587c4bbcaa2935c48d1c87610614410bdb
lucasxs2466 <130997187+lucasxs2466@users.noreply.github.com>
2024-04-22 11:58:05 -0300

    .

--- cont.js
+++ cont.js
@@ -18,9 +18,15 @@ console.log(i);
 
 const contadores = document.querySelectorAll('.contador');
 const TempoObjetivo1 = new Date ('2024-04-30T00:00:00');
+const TempoObjetivo2 = new Date ('2024-04-30T00:00:00');
+const TempoObjetivo3 = new Date ('2024-04-30T00:00:00');
+const TempoObjetivo4 = new Date ('2024-04-30T00:00:00');
 
+let Tempos = {TempoObjetivo1,TempoObjetivo2,TempoObjetivo3,TempoObjetivo4};
+
+function caalculaTempo(tempoObjetivo){
 let TempoAtual = new Date ();
-let tempo = TempoObjetivo1 - TempoAtual;
+let tempo = TempoObjetivo - TempoAtual;
 let segundos = Math.floor(TempoFinal/1000);
 let minutos = Math.floor(segundos/60);
 let horas = Math.floor(minutos/60);
@@ -30,4 +36,5 @@ segundos %=60;
 minutos %=60;
 horas %=24
 
-contadores[0].textContent = dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos";
+return dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos";
+}
